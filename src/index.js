b9e24a847c884249f6fc3c1e97bfc1581fa566b8
yzx0 <710740608@qq.com>
2021-01-31 19:00:51 +0800

    使用vue-cli的库模式打包

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export {default as Button} from './components/button.vue'
+export {default as ButtonGroup} from './components/button-group.vue'
+export {default as Icon} from './components/icon.vue'
