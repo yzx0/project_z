f2fa6456d18df40217c4e04f9c735401260ccd1e
yzx0 <710740608@qq.com>
2021-01-31 10:13:17 +0800

    使用travis ci 进行持续集成

--- tests/unit/button.spec.js
+++ tests/unit/button.spec.js
@@ -1,10 +1,63 @@
-import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
+import chai,{ expect } from 'chai'
+import { mount } from '@vue/test-utils'
+import sinonChai from 'sinon-chai'
 import Button from '../../src/components/button.vue'
-
+import sinon from 'sinon'
+chai.use(sinonChai)
 describe('button.vue',()=>{
   it('按钮组件存在',()=>{
-    const button = shallowMount(Button)
-    expect(button).to.exist
+    expect(Button).to.exist
+  })
+  it('可以设置icon',()=>{
+    const wrapper = mount(Button,{
+      propsData:{
+        icon:'setting'
+      }
+    })
+    const el = wrapper.vm.$el
+    expect(el.querySelector('use').getAttribute('xlink:href')).to.eq('#i-setting')
+  })
+  it('可以设置loading',()=>{
+    const wrapper = mount(Button,{
+      propsData:{
+        icon:'setting',
+        loading:true
+      }
+    })
+    const els = wrapper.vm.$el.querySelectorAll('use')
+    expect(els.length).to.eq(1)
+    expect(els[0].getAttribute('xlink:href')).to.eq('#i-loading')
+  })
+  it('icon默认在左边',()=>{
+    const wrapper = mount(Button, {
+      propsData: {
+        icon: 'setting',
+      }
+    })
+    const vm = wrapper.vm
+    expect(vm.$el.classList.contains('left')).to.eq(true)
+  })
+  it('可以通过iconPosition改变icon位置',()=>{
+    const wrapper = mount(Button,{
+      propsData:{
+        icon:'setting',
+        iconPosition:'right'
+      }
+    })
+    const vm = wrapper.vm
+    expect(vm.$el.classList.contains('right')).to.eq(true)
+  })
+  it('点击按钮出发 click事件',()=>{
+    const wrapper = mount(Button,{
+      propsData:{
+        icon:'setting',
+        iconPosition:'right'
+      }
+    })
+    const vm = wrapper.vm
+    const spy = sinon.spy()
+    vm.$on('click',spy)
+    vm.$el.click()
+    expect(spy).to.be.called
   })
 })
